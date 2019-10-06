c66f1981c9ffea15d1bc7883369af88f1eee90d4
842156727@qq.com <Wang643174>
2019-10-06 21:51:45 +0800

    减少包依赖, 去掉asm依赖

--- src/main/java/com/github/netty/core/util/AsmMethodToParameterNamesFunction.java
+++ src/main/java/com/github/netty/core/util/AsmMethodToParameterNamesFunction.java
@@ -1,16 +1,16 @@
-package com.github.netty.core.util;
-
-import java.lang.reflect.Method;
-import java.util.function.Function;
-
-/**
- * ASM - based method variable parameter name function
- * @author wangzihao
- */
-public class AsmMethodToParameterNamesFunction implements Function<Method,String[]> {
-    private ParameterNameDiscoverer parameterNameDiscoverer = new ParameterNameDiscoverer();
-    @Override
-    public String[] apply(Method method) {
-        return parameterNameDiscoverer.getParameterNames(method);
-    }
-}
+//package com.github.netty.core.util;
+//
+//import java.lang.reflect.Method;
+//import java.util.function.Function;
+//
+///**
+// * ASM - based method variable parameter name function
+// * @author wangzihao
+// */
+//public class AsmMethodToParameterNamesFunction implements Function<Method,String[]> {
+//    private ParameterNameDiscoverer parameterNameDiscoverer = new ParameterNameDiscoverer();
+//    @Override
+//    public String[] apply(Method method) {
+//        return parameterNameDiscoverer.getParameterNames(method);
+//    }
+//}
