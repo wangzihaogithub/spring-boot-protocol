ce518a77bf303888dc5c450e33843f866cf6f82b
wangzihaogithub <hao.wang@iterget.com>
2023-10-10 14:23:24 +0800

    强依赖降到按需依赖：io.netty.handler.codec.http2.*

--- src/test/java/com/github/netty/http/HttpBootstrap.java
+++ src/test/java/com/github/netty/http/HttpBootstrap.java
@@ -1,17 +1,10 @@
 package com.github.netty.http;
 
-import com.github.netty.core.util.LinkedMultiValueMap;
-import com.github.netty.protocol.servlet.util.ServletUtil;
 import com.github.netty.springboot.EnableNettyEmbedded;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.http.Cookie;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-
-import static com.github.netty.protocol.servlet.util.ServletUtil.*;
 
 @EnableNettyEmbedded
 @SpringBootApplication
@@ -20,14 +13,6 @@ public class HttpBootstrap {
             "/http/application.yaml");
 
     public static void main(String[] args) {
-        Cookie[] cookies = decodeCookie("1");
-        String dateByRfcHttp = getDateByRfcHttp();
-
-        LinkedMultiValueMap<String, String> objectObjectLinkedMultiValueMap = new LinkedMultiValueMap<>();
-        decodeByUrl(objectObjectLinkedMultiValueMap, "ew?qwq="+ URLEncoder.encode("s深深的 辅导"), Charset.defaultCharset());
-        System.out.println("objectObjectLinkedMultiValueMap = " + objectObjectLinkedMultiValueMap);
-
-//        NettyReportRunnable.start();
         System.getProperties().put("spring.config.location", CONFIG_URL.toString());
         SpringApplication.run(HttpBootstrap.class, args);
     }
